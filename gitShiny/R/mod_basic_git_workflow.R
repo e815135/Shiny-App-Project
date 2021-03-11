095431974392ff97a2c14d1f3f657ecb28ad552d
Elizabeth Brown <ebrown@Mango.local>
2021-03-11 16:02:50 +0000

    complete basic workflow page

--- gitShiny/R/mod_basic_git_workflow.R
+++ gitShiny/R/mod_basic_git_workflow.R
@@ -14,12 +14,22 @@ mod_basic_git_workflow_ui <- function(id){
     tagList(
       
       fluidRow(
-        shinydashboard::box(plotOutput(ns("plot"))),
-        shinydashboard::box(actionButton(ns("do"), "Click Me")))
+        shinydashboard::box(width = 12,
+                            title = "Basic Git Workflow",
+                            tags$div(
+                              "Below describes the stages of
+                              a basic Git workflow used for 
+                              version control: press the 'Next' and 'Previous'
+                               buttons to navigate through the stages.")),
+        shinydashboard::box(width = 7,
+                            imageOutput(ns("image"))),
+        shinydashboard::box(width = 12, 
+                            actionButton(ns("previous_button"), "Previous"),
+                            actionButton(ns("next_button"), "Next"))
+      )
+
     )
-      
-      
-  
+
 }
 
 #' basic_git_workflow Server Functions
@@ -28,15 +38,283 @@ mod_basic_git_workflow_ui <- function(id){
 mod_basic_git_workflow_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-    observeEvent(input$do, {
-      data(mtcars)
-      output$plot <- renderPlot(plot(mpg ~ disp, mtcars))
+    
+    # set counter
+    counter <- reactiveValues(countervalue = 0)
+    
+    # Initial image to be shown
+    output$image <- renderImage({
+      list(
+        src = file.path(
+          "R/images/basic_git_workflow/workflow_image_1.png"),
+        contentType = "image/png",
+        width = 700,
+        height = 400
+      )
+    }, deleteFile = FALSE)
+    
+    # When Next button is pressed:
+    
+    observeEvent(input$next_button, {
+      # If Next button pressed, increment by 1
+      counter$countervalue <- counter$countervalue + 1
+
+      # Depending on countervalue, render the appropiate image
+      if (counter$countervalue == 1){
+        
+      output$image <- renderImage({
+        list(
+          src = file.path(
+            "R/images/basic_git_workflow/workflow_image_2.png"),
+          contentType = "image/png",
+          width = 700,
+          height = 400
+        )
+      }, deleteFile = FALSE)
+      
+      } else if (counter$countervalue == 2) {
+        output$image <- renderImage({
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_3.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+      } else if (counter$countervalue == 3) {
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_4.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 4) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_5.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 5) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_6.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 6) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_7.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 7) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_8.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 8) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_9.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 9) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_10.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+        # cap after 9 clicks
+      } else if (counter$countervalue > 9) {
+        counter$countervalue <- 9
+      }    
+      
     })
+      
+      
+      # When Previous button is pressed:
+      
+      
+    observeEvent(input$previous_button, {
+      # If Previous button pressed, increment by -1
+      counter$countervalue <- counter$countervalue - 1
+      
+      # Depending on countervalue, render the appropiate image
+      if (counter$countervalue == 1){
+        
+      output$image <- renderImage({
+        list(
+          src = file.path(
+            "R/images/basic_git_workflow/workflow_image_2.png"),
+          contentType = "image/png",
+          width = 700,
+          height = 400
+        )
+      }, deleteFile = FALSE)
+      
+      } else if (counter$countervalue == 2) {
+        output$image <- renderImage({
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_3.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+      } else if (counter$countervalue == 3) {
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_4.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 4) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_5.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 5) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_6.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 6) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_7.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 7) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_8.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue == 8) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_9.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+      } else if (counter$countervalue >= 9) {
+        
+        output$image <- renderImage({
+          
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_10.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+        
+        # set lower bound for clicks
+      } else if (counter$countervalue <= 0){
+        counter$countervalue <- 0
+        output$image <- renderImage({
+          list(
+            src = file.path(
+              "R/images/basic_git_workflow/workflow_image_1.png"),
+            contentType = "image/png",
+            width = 700,
+            height = 400
+          )
+        }, deleteFile = FALSE)
+      }
+      
+    })
+    
 })
 }
-    
-## To be copied in the UI
-# mod_basic_git_workflow_ui("basic_git_workflow_ui_1")
-    
-## To be copied in the server
-# mod_basic_git_workflow_server("basic_git_workflow_ui_1")
